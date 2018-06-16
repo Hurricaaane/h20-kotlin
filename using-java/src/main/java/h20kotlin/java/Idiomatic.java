0f2cdd5f61674a1b5f5cec0935aef6e4a23cf9fc
Ha3 <git@ha3.eu>
2018-06-16 12:37:40 +0200

    Idiomatic

--- using-java/src/main/java/h20kotlin/java/Idiomatic.java
+++ using-java/src/main/java/h20kotlin/java/Idiomatic.java
@@ -8,6 +8,15 @@ public class Idiomatic {
     public static void main(String[] args) {
         List<Integer> numbers = Arrays.asList(7, 0, 60, 3, 6, 4, 30, 190);
 
+        int i = 0;
+        while (i < numbers.size()) {
+            Integer number = numbers.get(i);
+            if (number % 3 == 0) {
+                System.out.println(number / 3);
+            }
+            i++;
+        }
+
         Iterator<Integer> iterator = numbers.iterator();
         while (iterator.hasNext()) {
             Integer number = iterator.next();
@@ -23,12 +32,14 @@ public class Idiomatic {
             }
         }
 
+        // Idiomatic, imperative style
         for (Integer number : numbers) {
             if (number % 3 == 0) {
                 System.out.println(number / 3);
             }
         }
 
+        // Idiomatic, functional style
         numbers.stream()
                 .filter(number -> number % 3 == 0)
                 .map(number -> number / 3)
