198a9ccd810abd5abeafbf10cc2394eba10f3abc
Ha3 <git@ha3.eu>
2018-06-18 00:05:31 +0200

    Add a premade helper for the presentation

---
+++ using-kotlin/src/main/kotlin/h20kotlin/kotlin/Teamclass.kt
@@ -0,0 +1,14 @@
+package h20kotlin.kotlin
+/*
+
+class Team(val name: String) {
+    private val members: MutableSet<String> = mutableSetOf()
+    private val relationships: MutableList<Relationship> = mutableListOf()
+
+    override fun toString(): String {
+        return (listOf("In $name team made of ${members.sorted()}")
+                + relationships).joinToString("\n")
+    }
+}
+
+*/
